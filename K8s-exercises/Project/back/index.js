c42b9b0ca1bcedec2249a0bbed81efa26d9a9c57
Wilson de Souza Pereira <raskholnikov@gmail.com>
2024-01-13 18:22:04 -0300

    Exercise 1.02: Project v0.1

---
+++ K8s-exercises/Project/back/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const {  connectToDatabase} = require('./util/db')
+const { PORT } = require('./util/config')
+
+
+const todosRouter = require('./controllers/todos')
+
+app.use(express.json())
+
+app.use('/api/todos', todosRouter)
+
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+  
+  console.log(`Server running on port ${PORT}`)
+  })
+
+}
+
+start()
+  
