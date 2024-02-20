a7c5f14e990f320e4fdb1fae49f0a6e6eef0acc1
Wilson de Souza Pereira <raskholnikov@gmail.com>
2024-02-20 10:37:17 -0300

    Exercise 2.03: Keep them separated

--- K8s-exercises/Log output/log-output/index.js
+++ K8s-exercises/Log output/log-output/index.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const { v4: uuidv4 } = require('uuid')
 const app = express()
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3001
 
 
 
 app.get('/', (req, res) => {
-  res.send(`${new Date().toISOString()} : ${uuidv4()}`)
+	res.send(`${new Date().toISOString()} : ${uuidv4()}`)
 })
 
 const start = async () => {
+    
+	app.listen(PORT, () => {
   
-  app.listen(PORT, () => {
-  
-  console.log(`Server running on port ${PORT}`)
-  })
+		console.log(`Server running on port ${PORT}`)
+	})
 
 }
 
